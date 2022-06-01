4cfcb59e6e2993501a48f0ba6cd11334ea1bcf9c
khanshorif331 <khanshorif331@gmail.com>
2022-06-01 23:00:41 +0600

    particles js added too my project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import React, { useCallback } from 'react';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import logo from './logo.svg';
-import './App.css';
-import particlesOptions from "./particles.json";
+import React, { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import logo from './logo.svg'
+import './App.css'
+import particlesOptions from './particles.json'
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+	const particlesInit = useCallback(main => {
+		loadFull(main)
+	}, [])
 
-    return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Particles options={particlesOptions} init={particlesInit} />
+			<h1 style={{ color: 'red' }}>Hello World Love from shorif</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
