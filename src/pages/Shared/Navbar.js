1ca0c70a4d92a6f389bad0a88d9bfc13e539e6f3
khanshorif331 <khanshorif331@gmail.com>
2022-06-07 23:18:08 +0600

    updated navbar

--- src/pages/Shared/Navbar.js
+++ src/pages/Shared/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({ children }) => {
 			<input id='my-drawer-3' type='checkbox' class='drawer-toggle' />
 			<div class='drawer-content flex flex-col'>
 				{/* <!-- Navbar --> */}
-				<div class='w-full navbar bg-base-300'>
+				<div class='w-full navbar bg-black sticky top-0 z-10'>
 					<div class='flex-none lg:hidden'>
 						<label for='my-drawer-3' class='btn btn-square btn-ghost'>
 							<svg
@@ -55,9 +55,9 @@ const Navbar = ({ children }) => {
 							</svg>
 						</label>
 					</div>
-					<div class='flex-1 px-2 mx-2'>Navbar Title</div>
+					<div class='flex-1 px-2 mx-2 text-white'>Navbar Title</div>
 					<div class='flex-none hidden lg:block'>
-						<ul class='menu menu-horizontal'>
+						<ul class='menu menu-horizontal text-white'>
 							{/* <!-- Navbar menu content here --> */}
 							{navigation}
 						</ul>
